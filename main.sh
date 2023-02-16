6556afa11e50bcf161f232086e78790d2e1e50ca
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2023-02-16 20:48:58 +0900

    feat: Homebrew のパッケージをインストールする処理を追加

--- main.sh
+++ main.sh
@@ -1,5 +1,13 @@
-#Install xcode-select
-xcode-select --install
-
 # Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if type "brew" > /dev/null 2>&1; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Update .Brewfile
+rm -f ~/.Brewfile
+cp .Brewfile ~/.Brewfile
+
+# Install Homebrew packages
+brew tap homebrew/cask-fonts
+brew update
+brew bundle --global
