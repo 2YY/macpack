85e3465abf99aaf0243e82051cc394904f0453bf
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2023-07-13 00:31:57 +0900

    feat: fishの設定ファイルを作成

---
+++ config.fish
@@ -0,0 +1,11 @@
+# ターミナル起動時にウェルカムメッセージを表示しない。
+set fish_greeting
+
+# タブキーで自動補完を採用する。
+bind \t accept-autosuggestion
+
+# Kiex を使えるようにする。
+test -s "$HOME/.kiex/scripts/kiex.fish"; and source "$HOME/.kiex/scripts/kiex.fish"
+
+# Starship を起動する。
+starship init fish | source
