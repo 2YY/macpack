fb11090223244bbc917aa23178031fb95a992f53
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2023-11-10 21:42:36 +0900

    feat: kiex関連の処理削除
    rtxにまとめたい

--- config.fish
+++ config.fish
@@ -4,9 +4,6 @@ set fish_greeting
 # タブキーで自動補完を採用する。
 bind \t accept-autosuggestion
 
-# Kiex を使えるようにする。
-test -s "$HOME/.kiex/scripts/kiex.fish"; and source "$HOME/.kiex/scripts/kiex.fish"
-
 # Starship を起動する。
 starship init fish | source
 
